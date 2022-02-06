36b0ac7dd53a44640a3e4a5cd42658b71c255f41
sia162 <nowitissiya@gmail.com>
2022-02-06 16:37:34 +0530

    feature: added local storage

--- README.md
+++ README.md
@@ -4,20 +4,16 @@ _a simple app to mark your reading journey_
 
 ---
 
-## Tech used
+## Tech used?
 
 1. HTML/CSS
 2. React.js
-
-<br>
 
 ## What it does?
 
 1. Add the books you are reading.
 2. Remove the books you've read.
 3. Data will be stored in local storage.
-
-<br>
 
 ## What I learned?
 
@@ -28,11 +24,13 @@ _a simple app to mark your reading journey_
    - useContext
    - useReducer
 
-<br>
-
 2. Revised the concepts of local storage and used implemented this in the app.
 
 ## Future of this application?
 
 1. Finished Books section.
 2. DNF Books section.
+3. API that suggests new books.
+4. Date of read and date of finish.
+5. Edit book option.
+6. Delete Book option.
--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,12 +1,19 @@
-import React, { createContext, useReducer, useState } from "react";
+import React, { createContext, useEffect, useReducer, useState } from "react";
 import { bookReducer } from "../reducers/BookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localDATA = localStorage.getItem("books");
+    return localDATA ? JSON.parse(localDATA) : [];
+  });
 
-  //addbook
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
+
+  //finished-books
   const [booksfinished, setBooksfinished] = useState([]);
   const finishedBook = (id) => {
     setBooksfinished(books.filter((book) => book.id === id));
